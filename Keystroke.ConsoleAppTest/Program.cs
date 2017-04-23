ec30929b3add3abbfcbd3e81a9cc4fc0b608ea45
Nathan Bean <natejbean@gmail.com>
2017-04-22 20:00:08 -0400

    made a system tray ap

--- Keystroke.ConsoleAppTest/Program.cs
+++ Keystroke.ConsoleAppTest/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using SimpleTextExpander;
+using SimpleTextExpander.Properties;
+using System;
 using System.Windows.Forms;
 
 namespace SimpleTextExpander
@@ -7,9 +10,41 @@ namespace SimpleTextExpander
     {
         static void Main(string[] args)
         {
-            KeyHandler ourKeyHandler = new KeyHandler();
 
-            Application.Run();
+            //Application.Run();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new TheAppContext());
         }
     }
 }//namespace
+
+
+
+public class TheAppContext : ApplicationContext
+{
+    private NotifyIcon trayIcon;
+
+    public TheAppContext()
+    {
+        KeyHandler ourKeyHandler = new KeyHandler();
+        // Initialize Tray Icon
+        trayIcon = new NotifyIcon()
+        {
+            Icon = Resources.AppIcon,
+            ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Exit", Exit)
+            }),
+            Visible = true
+        };
+    }
+
+    void Exit(object sender, EventArgs e)
+    {
+        // Hide tray icon, otherwise it will remain shown until user mouses over it
+        trayIcon.Visible = false;
+
+        Application.Exit();
+    }
+}
